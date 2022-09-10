2e3cefe3b55fa4cd062d21d3d8b46816e64f8c18
ghadeeralm <ghaderalm@outlook.com>
2022-09-10 19:32:08 +0300

    part1

---
+++ cw3 part1/cw3 part1/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  cw3 part1
+//
+//  Created by ghadeer almajed on 10/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var grade = ""
+    @State var tap = ""
+    
+    
+    var body: some View {
+        //Text("Hello, world!")
+            //.padding()
+        ZStack{
+            Color.teal
+                .ignoresSafeArea()
+            VStack{
+                Text("حاسب الدرجات")
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    
+                Image("calc")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 200)
+                TextField("Enter Your Grade", text:  $grade)
+                Text("احسب درجتي")
+                   // .padding()
+                    .foregroundColor(.white)
+                    .font(.system(size: 16))
+                    .frame(width: 90, height: 40)
+                    .background(Color.yellow)
+                    .clipShape(Capsule())
+            
+                    .onTapGesture {
+                        if Double(grade) ?? 0 >= 90
+                        {
+                            tap = "امتياز"
+                        }
+                            else if Double(grade) ?? 0 >= 80 && Double(grade) ?? 0 < 90
+                            {
+                                tap = "جيد جدا"
+                            }
+                            else if Double(grade) ?? 0 >= 70 && Double(grade) ?? 0 < 80
+                                {
+                                tap = "جيد"
+                                }
+                        else if Double(grade) ?? 0 >= 60 && Double(grade) ?? 0 < 70
+                                {
+                                    tap = "مقبول"
+                                }
+                                else{
+                                    tap = "راسب"
+                                }
+                            
+                    }
+                        
+                Text("لقد حصلت على درجة:")
+              
+                Text(tap)
+                    }
+            
+                
+                
+                
+                
+            }
+            
+            
+        }
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw3 part1/cw3 part1/cw3_part1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw3_part1App.swift
+//  cw3 part1
+//
+//  Created by ghadeer almajed on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw3_part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
